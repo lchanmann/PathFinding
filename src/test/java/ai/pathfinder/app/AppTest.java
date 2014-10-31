b91423fc730faf82e11b107e2314ce96d22dab67
Mann <chanmannlim@gmail.com>
2014-10-31 17:47:29 -0500

    add dummy test

---
+++ src/test/java/ai/pathfinder/app/AppTest.java
@@ -0,0 +1,12 @@
+package ai.pathfinder.app;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void dummyTest() {
+        Assert.assertTrue(true);
+    }
+}
