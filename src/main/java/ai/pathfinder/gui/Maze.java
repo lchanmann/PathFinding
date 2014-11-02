c7e6f58aa080500aacc973a5162795046eacd2de
Mann <chanmannlim@gmail.com>
2014-11-02 10:58:21 -0600

    draw and erase wall

--- src/main/java/ai/pathfinder/gui/Maze.java
+++ src/main/java/ai/pathfinder/gui/Maze.java
@@ -7,6 +7,10 @@ import java.awt.Point;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import javax.swing.JComponent;
 
@@ -20,8 +24,11 @@ public class Maze extends JComponent implements MouseMotionListener,
     private final int height = 22;
 
     private final Point startingPoint = new Point(250, 250);
-    private Point goalPoint = new Point(550, 250);
+    private final Point goalPoint = new Point(550, 250);
     private Point selectedPoint;
+    private final Set<Point> wall = new HashSet<Point>();
+    private boolean drawingWall;
+    private boolean erasingWall;
 
     public Maze() {
         addMouseListener(this);
@@ -39,12 +46,19 @@ public class Maze extends JComponent implements MouseMotionListener,
 
         drawMaze(g2);
 
+        drawWall(g2);
         drawTarget(g2, startingPoint, Color.GREEN);
         drawTarget(g2, goalPoint, Color.RED);
     }
 
+    private void drawWall(Graphics2D g2) {
+        for (Point block : wall) {
+            drawTarget(g2, block, Color.GRAY);
+        }
+    }
+
     private void drawMaze(Graphics2D g2) {
-        g2.setColor(Color.GRAY);
+        g2.setColor(Color.LIGHT_GRAY);
         // horizontal lines
         for (int i = 0; i <= height; i++) {
             g2.drawLine(0, i * blockSize, width * blockSize, i * blockSize);
@@ -59,7 +73,7 @@ public class Maze extends JComponent implements MouseMotionListener,
         g2.setColor(color);
         g2.fillRect(position.x, position.y, blockSize, blockSize);
 
-        g2.setColor(Color.GRAY);
+        g2.setColor(Color.LIGHT_GRAY);
         g2.drawRect(position.x, position.y, blockSize, blockSize);
     }
 
@@ -67,12 +81,17 @@ public class Maze extends JComponent implements MouseMotionListener,
     public void mouseDragged(MouseEvent e) {
         int x = e.getX();
         int y = e.getY();
+        Point dragged = snapToGrid(x, y);
 
         if (selectedPoint != null) {
             if (x > 0 && y > 0 &&
                     x < getWidth() && y < getHeight()) {
-                selectedPoint.setLocation(snapToGrid(x, y));
+                selectedPoint.setLocation(dragged);
             }
+        } else if (drawingWall) {
+            wall.add(dragged);
+        } else if (erasingWall) {
+            wall.remove(dragged);
         }
         repaint();
     }
@@ -95,11 +114,20 @@ public class Maze extends JComponent implements MouseMotionListener,
 
         if (pressed.distance(startingPoint) == 0) selectedPoint = startingPoint;
         else if (pressed.distance(goalPoint) == 0) selectedPoint = goalPoint;
+        else {
+            if (wall.contains(pressed)) {
+                erasingWall = true;
+            } else {
+                drawingWall = true;
+            }
+        }
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
         selectedPoint = null;
+        drawingWall = false;
+        erasingWall = false;
     }
 
     @Override
