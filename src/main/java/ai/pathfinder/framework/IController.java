670e1685bea94e86a52cb44274fbe08cbc4f6044
Mann <chanmannlim@gmail.com>
2014-11-02 01:08:32 -0600

    add comments

--- src/main/java/ai/pathfinder/framework/IController.java
+++ src/main/java/ai/pathfinder/framework/IController.java
@@ -2,10 +2,19 @@ package ai.pathfinder.framework;
 
 public interface IController {
 
+    /**
+     * Initialize controller
+     */
     public void init();
 
+    /**
+     * Search
+     */
     public void search();
 
+    /**
+     * Reset
+     */
     public void reset();
 
 }
--- src/main/java/ai/pathfinder/framework/IMainView.java
+++ src/main/java/ai/pathfinder/framework/IMainView.java
@@ -2,10 +2,20 @@ package ai.pathfinder.framework;
 
 public interface IMainView {
 
+    /**
+     * Initialize view
+     * @param controller
+     */
     public void init(IController controller);
 
+    /**
+     * Start search from view
+     */
     public void startSearch();
 
+    /**
+     * Reset view
+     */
     public void resetView();
 
 }
